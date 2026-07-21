0251e574e8802bc168e0bc6726b1976ad0dc2b73
Andrew Bergman <abergman804@gmail.com>
2026-07-21 12:10:06 -0400

    Created interactive main menu

---
+++ src/main.py
@@ -0,0 +1,31 @@
+def main():
+    print("-----------------")
+    print("Main Menu")
+    print("-----------------")
+    print("1. View song database")
+    print("2. Add song")
+    print("3. Choose next song")
+    print("4. Exit")
+
+    while True: 
+
+        choice = input("Select an option: ")
+        if choice == "1":
+            pass
+
+        elif choice == "2":
+            pass
+
+        elif choice == "3":
+            pass
+
+        elif choice == "4":
+            print("End")
+            break
+
+        else:
+            print("Invalid option. Please try again.")
+
+    
+if __name__ == "__main__":
+    main()
